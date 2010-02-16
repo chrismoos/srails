ef6209e946d83f157faa5157f0d4150791978569
Chris Moos <chris@tech9computers.com>
2010-02-15 17:46:58 -0700

    removed extra files

--- src/main/scala/com/cmoos/srails/servlet/MainServlet.scala
+++ src/main/scala/com/cmoos/srails/servlet/MainServlet.scala
@@ -3,8 +3,6 @@ package com.cmoos.srails.servlet
 import javax.servlet._
 import javax.servlet.http._
 
-import com.cmoos.demo._
-
 class MainServlet extends HttpServlet {  
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
     Router.handleURI(req, resp)
